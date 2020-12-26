13413c2d435652ef93144f5dff451ba62d2a43fc
NavaneethSankar44 <75898364+NavaneethSankar44@users.noreply.github.com>
2020-12-26 17:10:46 +0530

    Add files via upload

---
+++ positive integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+range1 = [12,-7,5,64,-14]
+range2 = [12,14,-95,3]
+n = 0 
+while(n < len(range1)): 
+    if range1[n] >= 0: 
+        print(range1[n], end = " ") 
+    n+=1
+n=0
+print("\n")
+while(n < len(range2)): 
+    if range2[n] >=0:
+        print(range2[n], end =" ")
+    n+=1
+
+
+# In[ ]:
+
+
+
+
